419b6e34c82f1e12f7bc61ac52148e2e8833822e
DanielMrt <daniel.martins@gordon.edu>
2014-09-11 17:02:32 -0500

    Tentativa

--- wall1.py
+++ wall1.py
@@ -4,8 +4,14 @@ Created on Thu Sep 04 17:23:12 2014
 
 @author: danie_000
 """
-
+#Primeiro passo, transfomar tudo em verde
+#Criar bolas como a expressao = X**2 + Y**2 = R**2 // sendo R um numero de principio fixo, mas depois colocar um numero randomico limitado.
+#Colocar as bolas em cores diferentes
 import pylab as p
+import math as mt
+import random
+import matplotlib.pyplot as plt
+
 fx=400 #(tamanho da matriz)
 fy=400
 x1=100 #(comessar alterar valor)
@@ -14,11 +20,13 @@ grad=p.ones((fx,fy))
 for i in range(x1,x2):
 	for j in range(fy):
 		grad[j][i]*=p.rand()
+for i in range (400):
+    a= random.randint(100,200)
+    b= random.randint(0,400)
+    r= random.randint(10,20)
+    
+##    circle1=plt.Circle((a,b),r,color='white')
+##fig = plt.gcf()
+##fig.gca().add_artist(circle1)
+##fig.savefig('plotcircles.png')
 p.contour(grad)
-p.show()
-
-#Testar metodo de circulos - atribuir valor 1 de ar, 2 pra numeros <0.9 e 3 pra > 0.9
-#Criar uma ooutra matriz pra encontrar o numeros >3 e criar o raio
-
-#Criar uma matriz parar suavizar os numeros fazendo as medias
-# E transformar uma faixa em ar.
